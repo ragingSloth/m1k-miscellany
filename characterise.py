255d6f30fc868bde4edb822c3a91d4a687a384ab
root <ez@sloth.life>
2015-01-27 03:41:44 +0000

    add binary / depens for operation on armhf, add logging to characterise.py

--- characterise.py
+++ characterise.py
@@ -1,50 +1,48 @@
 import usb
 import time
 import numpy as np
+import pprint
+import pickle
 from libpysmu import smu
 
 device = smu()
 d = usb.core.find(idVendor=0x064b,idProduct=0x784c)
-print device.devices[0][0]
+uid = device.devices[0][0]
+
+saveData = {'uid': uid}
 for name,chan in device.chans.items():
-	
-	svmiPoints = np.linspace(0, 5, 500)
+	svmiPoints = np.linspace(0, 5, 50)
 	data = []
 	for vval in svmiPoints:
 		chan.set_mode('v')
 		chan.constant(vval)
-		newData = chan.get_samples(500)
+		newData = chan.get_samples(50)
 		data += [[np.mean([x[0] for x in newData]), np.mean([x[1] for x in newData])]]
 	
 	data = np.array(data).T
 	currentOffset = np.mean(data[1])
-	print 'current Offset', name, currentOffset
-
+	saveData['current offset '+name] = currentOffset
 	chan.set_mode('d')
 	if name == 'A':
 		d.ctrl_transfer(0x40,0x50,32,0,0)
 	if name == 'B':
 		d.ctrl_transfer(0x40,0x50,37,0,0)
-	newData = chan.get_samples(500)
+	newData = chan.get_samples(50)
 	voltageOffset = np.mean([x[0] for x in newData])
-	print 'voltage Offset', name, voltageOffset
-	
-	simvPoints = np.linspace(-0.04, 0.04, 500)
-	
+	saveData['voltage offset '+name] = voltageOffset
+	simvPoints = np.linspace(-0.04, 0.04, 50)
 	data = []
 	for ival in simvPoints:
 		chan.set_mode('i')
 		chan.constant(ival)
-		newData = chan.get_samples(500)
-		data += [[np.mean([x[0] for x in newData]), np.mean([x[1] for x in newData])]]
-
+		newData = chan.get_samples(50)
+		data += [[ival, np.mean([x[0] for x in newData]), np.mean([x[1] for x in newData])]]
 	data = np.array(data).T
-
+	saveData['current source offset '+name] = data
 	if name == 'A':
 		d.ctrl_transfer(0x40,0x51,32,0,0)
 	if name == 'B':
 		d.ctrl_transfer(0x40,0x51,37,0,0)
 
-#from pylab import *
-#plot(simvPoints, data[1]-simvPoints, '.')
-#show()
+pprint.pprint(saveData)
+pickle.dump(saveData, open(uid+'+'+str(int(time.time()))+'.p', 'w'))
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import time
 
 def bootload():
     dev = usb.core.find(idVendor=0x064b, idProduct=0x784c)
+    if not dev:
+         dev = usb.core.find(idVendor=0x0456, idProduct=0x784c)
     if dev:
          try:
              dev.ctrl_transfer(0x40|0x80, 0xBB, 0, 0, 1)
@@ -13,15 +15,10 @@ def bootload():
 
 pio = lambda x: subprocess.Popen(["pio", "-m", x])
 
-
-bootload()
 while True:
    while not usb.core.find(idProduct=0x6124, idVendor=0x03eb):
       time.sleep(1)
-   # wait for user input, remove this eventually
-   # should check for retval of 1
-   bootload()
-   time.sleep(1)
+      bootload()
    flash_pipe = subprocess.Popen(["python", "sam-ba.py"])
    flash_pipe.wait()
    pio("PB9*1")
@@ -31,6 +28,7 @@ while True:
    if usb.core.find(idVendor=0x064b,idProduct=0x784c):
        char_pipe = subprocess.Popen(["python", "characterise.py"])
        char_pipe.wait()
+       print 'characterisation complete'
        for i in range(4):
    	       pio("PH24*1")
    	       time.sleep(1)
